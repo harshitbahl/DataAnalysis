ff5264ce516f0aa2789e10d04736d59243687eca
harshitbahl <harshit.bahl@me.com>
2014-06-23 23:25:48 -0500

    firstcut

--- Chapter1.py
+++ Chapter1.py
@@ -4,41 +4,25 @@ Created on Mon Jun 23 22:00:19 2014
 
 @author: harshitbahl
 """
-import scipy
-import numpy
-import sys
-import matplotlib.pyplot
+
 def main():
-    if(len(sys.argv) != 3):
-       print "Usage python %s yfactor xfactor" % (sys.argv[0])
-       sys.exit()
-    
-    # Loads the Lena image into an array
-    lena = lena = scipy.imag('/Users/harshitbahl/Pictures/IMG_1550 1.JPG')
-    #Lena's dimensions
-    LENA_X = 512
-    LENA_Y = 512
-    #Check the shape of the Lena array
-#    numpy.testing.assert_equal((LENA_X, LENA_Y), lena.shape)
-    
-    # Get the resize factors
-    yfactor = float(sys.argv[1])
-    xfactor = float(sys.argv[2])
-    
-    # Resize the Lena array
-    resized = lena.repeat(yfactor, axis=0).repeat(xfactor, axis=1)
-    
-    #Check the shape of the resized array
-    numpy.testing.assert_equal((yfactor * LENA_Y, xfactor * LENA_Y), resized.shape)
-    
-    # Plot the Lena array
-    matplotlib.pyplot.subplot(211)
-    matplotlib.pyplot.imshow(lena)
-    
-    #Plot the resized array
-    matplotlib.pyplot.subplot(212)
-    matplotlib.pyplot.imshow(resized)
-    matplotlib.pyplot.show()
+    import json
+    path = '../pydata-book/ch02/usagov_bitly_data2012-03-16-1331923249.txt'
+    records = [json.loads(row) for row in open(path)]
+    records[0]
+    from pandas import DataFrame, Series
+    import pandas as pd; import numpy as np
+    frame = DataFrame(records)
+    frame
+    tz_counts = frame['tz'].value_count()
+    tz_counts = frame['tz'].value_counts()
+    tz_counts[:10]
+    clean_tz = frame['tz'].fillna('Missing')
+    clean_tz[clean_tz == '']= 'Unknow'
+    tz_counts = clean_tz.value.counts()
+    tz_counts = clean_tz.value_counts()
+    tz_counts[:10]
+    tz_counts[:10].plot(kind='barh', rot=0)
 
     
     
