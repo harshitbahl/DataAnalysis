5cd8b41e642033bfc63c8211ebafa71a6222cd58
harshitbahl <harshit.bahl@me.com>
2014-06-23 22:14:46 -0500

    first cut

---
+++ Chapter1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 23 22:00:19 2014
+
+@author: harshitbahl
+"""
+import scipy
+
+def main():
+    
+    
+
+
+
+
+
+if __name__ == '__main__':
+    main()
